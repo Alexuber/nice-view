0e843fd0f7022a8b9e2b915bbf53a43371bdbb3f
Alexuber <alexuberbuber@gmail.com>
2022-12-01 21:24:50 +0200

    comit1

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -6,8 +6,9 @@ const swiper = new Swiper('.mySwiper', {
    modules: [Navigation, Pagination],
   // Optional parameters
   slidesPerView: 3,
-  spaceBetween: 0,
+  slidesPerGroup: 1,
   loop: true,
+  loopFillGroupWithBlank: true,
   
 
   // If we need pagination
@@ -22,3 +23,5 @@ const swiper = new Swiper('.mySwiper', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+
