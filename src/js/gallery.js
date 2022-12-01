1ac753664f2d9e00c3106950d253e40994859862
Alexuber <alexuberbuber@gmail.com>
2022-12-02 00:05:18 +0200

    adaptive slider ok

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -7,10 +7,32 @@ const swiper = new Swiper('.mySwiper', {
   // Optional parameters
   slidesPerView: 3,
   slidesPerGroup: 1,
+  spaceBetween: 0,
   loop: true,
   loopFillGroupWithBlank: true,
-  
+  grabCursor: true,
+  slideToClickedSlide: true,
+  // keyboard: {
+  //   enabled: true,
+  //   onlyInViewport:true,
+  //   pageUpDown:true,
+  // },
+  autoplay: {
+    delay: 1000,
+    stopOnLastSlide: true,
+    disableOnInteraction: false,
+  },
 
+  speed: 900,
+
+  // effect: 'coverflow',
+  // coverflowEffect: {
+  //   rotate:20,
+  //   stretch:20,
+  //   slideShadows:true,
+  // },
+
+  
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
